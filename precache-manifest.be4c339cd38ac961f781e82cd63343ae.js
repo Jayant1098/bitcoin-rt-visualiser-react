76d1f6774770628102118fb1e8dbbada5149c88e
Jayant1098 <jayantpahuja9@gmail.com>
2020-06-24 19:02:12 +0530

    Updates

---
+++ precache-manifest.be4c339cd38ac961f781e82cd63343ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f23294d985a008664116e2e7b9d4844",
+    "url": "/bitcoin-rt-visualiser-react/index.html"
+  },
+  {
+    "revision": "9127c390d65f45def9d1",
+    "url": "/bitcoin-rt-visualiser-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "173b6856c65047561bb6",
+    "url": "/bitcoin-rt-visualiser-react/static/js/2.afb09a8a.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/bitcoin-rt-visualiser-react/static/js/2.afb09a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9127c390d65f45def9d1",
+    "url": "/bitcoin-rt-visualiser-react/static/js/main.91dfcc7f.chunk.js"
+  },
+  {
+    "revision": "8131131ac6099f2affb4",
+    "url": "/bitcoin-rt-visualiser-react/static/js/runtime-main.0ecdb963.js"
+  }
+]);
